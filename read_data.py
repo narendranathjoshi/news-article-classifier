2dd79705ceea990fc8bb091835f14400074f1e49
Narendra Nath Joshi <nnj@cs.cmu.edu>
2016-11-23 16:22:05 -0500

    Added file reading module for *.dat files

---
+++ read_data.py
@@ -0,0 +1,9 @@
+def read_data(filename):
+    text = open(filename).read().split("~~~~~")
+    text = [line.strip().split("\n") for line in filter(None, text)]
+
+    return text
+
+
+if __name__ == "__main__":
+    print read_data("trainingSet.dat")
