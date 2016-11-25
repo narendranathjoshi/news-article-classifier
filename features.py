fd1faf9c3ccf550ff559707ef912ae5c0481101a
Narendra Nath Joshi <nnj@cs.cmu.edu>
2016-11-25 13:50:04 -0500

    Add sentence length mean feature class

--- features.py
+++ features.py
@@ -1,1 +1,21 @@
+from __future__ import division
+from sklearn.base import TransformerMixin
+
+
+class SentenceLengthMeanFeature(TransformerMixin):
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, **transform_params):
+        means = []
+        total_length = 0
+        for article in X:
+            for sentence in article:
+                total_length += len(sentence)
+
+            mean = total_length / len(X)
+            means.append(mean)
+
+        return means
+
 
