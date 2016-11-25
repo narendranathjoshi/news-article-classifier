187aa34a669fdd98b7a18f38ba23d7ee6eaaaab6
Narendra Nath Joshi <nnj@cs.cmu.edu>
2016-11-25 14:50:01 -0500

    Add Flesch-Kincaid ease of readability score feature

--- features.py
+++ features.py
@@ -4,6 +4,8 @@ from collections import Counter
 
 from sklearn.base import TransformerMixin
 
+import utils
+
 
 class SentenceLengthMeanFeature(TransformerMixin):
     def fit(self, X, y=None, **fit_params):
@@ -38,5 +40,25 @@ class SentenceLengthModeFeature(TransformerMixin):
             modes.append(Counter(lengths).most_common(1)[0][0])
 
         return modes
+
+
+class FleschKincaidReadabilityEaseFeature(TransformerMixin):
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, **transform_params):
+        ease_scores = []
+        for article in X:
+            number_of_sentences = len(article)
+            number_of_words = 0
+            number_of_syllables = 0
+            for sentence in article:
+                number_of_words += len(sentence)
+                number_of_syllables += sum(map(lambda x: utils.count_syllables(x), sentence))
+
+            ease_scores.append(
+                utils.flesch_kincaid_ease_score(number_of_sentences, number_of_words, number_of_syllables)
+            )
+        return ease_scores
 
 
