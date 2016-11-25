72c744216b9fb817698943df8e81b7c6d8423a63
Narendra Nath Joshi <nnj@cs.cmu.edu>
2016-11-25 13:54:12 -0500

    Add sentence length mode feature

--- features.py
+++ features.py
@@ -1,4 +1,7 @@
 from __future__ import division
+
+from collections import Counter
+
 from sklearn.base import TransformerMixin
 
 
@@ -17,5 +20,23 @@ class SentenceLengthMeanFeature(TransformerMixin):
             means.append(mean)
 
         return means
+
+
+class SentenceLengthModeFeature(TransformerMixin):
+    def fit(self, X, y=None, **fit_params):
+        return self
+
+    def transform(self, X, **transform_params):
+        modes = []
+        total_length = 0
+        for article in X:
+            lengths = []
+            for sentence in article:
+                total_length += len(sentence)
+
+            lengths.append(total_length)
+            modes.append(Counter(lengths).most_common(1)[0][0])
+
+        return modes
 
 
