5d5cf79ca3e544e851aad30cc6327e813395c0c8
Narendra Nath Joshi <nnj@cs.cmu.edu>
2016-11-26 12:07:59 -0500

    Modularize program workflow

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from sklearn.pipeline import Pipeline,FeatureUnion
-from features import FleschKincaidReadabilityEaseFeature,SentenceLengthMeanFeature,\
-    JaccardSimilarityAverageFeature,SentenceLengthModeFeature,DummyEstimator
+from sklearn.pipeline import Pipeline, FeatureUnion
+from features import FleschKincaidReadabilityEaseFeature, SentenceLengthMeanFeature, \
+    JaccardSimilarityAverageFeature, SentenceLengthModeFeature, DummyEstimator
 from read_data import load_data
 from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
@@ -11,9 +11,11 @@ from sklearn.metrics import accuracy_score
 Option to cross-validate training data
 Useful to perform a bias/variance check
 """
-def cross_validate(pipeline,data,cv=4):
+
+
+def cross_validate(pipeline, data, cv=4):
     print "Running cross validation..."
-    (Xcv,ycv) = data
+    (Xcv, ycv) = data
     kfold = KFold(n_splits=cv, shuffle=True, random_state=42)
     results = []
     for train_idx, val_idx in kfold.split(Xtrain):
@@ -23,21 +25,24 @@ def cross_validate(pipeline,data,cv=4):
         ))
     print "{} +/- {}".format(np.mean(results), np.std(results))
 
+
 if __name__ == '__main__':
     print "Importing data..."
-    Xtrain,ytrain = load_data("train")
-    Xdev,ydev = load_data("dev")
+    Xtrain, ytrain = load_data("train")
+    Xdev, ydev = load_data("dev")
     nac_pipeline = Pipeline([
         ('features', FeatureUnion([
-            ('sent_len_mean', SentenceLengthMeanFeature()),
-            ('fs_kinc_score',FleschKincaidReadabilityEaseFeature()),
-            # ('jacc_sim',JaccardSimilarityAverageFeature()) : Buggy
-            # ('sent_len_mode', SentenceLengthModeFeature()), : Buggy
+            ('sentence_length_mean', SentenceLengthMeanFeature()),
+            ('flesch_kincaid_score', FleschKincaidReadabilityEaseFeature()),
+            ('jaccard_similarity',JaccardSimilarityAverageFeature()),
+            ('sentence_length_mode', SentenceLengthModeFeature()),
         ])),
-        ('logreg_clf',LogisticRegression())
+        ('logreg_clf', LogisticRegression())
     ])
+
     # cross_validate(nac_pipeline,(Xtrain,ytrain),4)
     print "Training..."
-    nac_pipeline.fit(Xtrain,ytrain)
+    nac_pipeline.fit(Xtrain, ytrain)
+
     print "Running on Development set..."
-    print accuracy_score(ydev,nac_pipeline.predict(Xdev))
+    print accuracy_score(ydev, nac_pipeline.predict(Xdev))
