cb8255b7ba0f28b4386cd4d139070e33eb73fd28
Narendra Nath Joshi <nnj@cs.cmu.edu>
2016-11-26 12:08:46 -0500

    Fix indent mishaps

--- features.py
+++ features.py
@@ -10,14 +10,19 @@ import utils
 """
 Use this to inspect X,y transformed values
 """
-class DummyEstimator:
 
-    def fit(self,X,y):
+
+class DummyEstimator:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
         print X[:5]
         return self
 
-    def predict(self,X):
+    def predict(self, X):
         return np.zeros(X.shape[0])
+
 
 class SentenceLengthMeanFeature(TransformerMixin):
     def fit(self, X, y=None, **fit_params):
@@ -34,9 +39,9 @@ class SentenceLengthMeanFeature(TransformerMixin):
             mean = total_length / len(X)
             means.append(mean)
 
-        return np.array(means).reshape(len(means),1)
+        return np.array(means).reshape(len(means), 1)
 
-#Bug in this feature
+
 class SentenceLengthModeFeature(TransformerMixin):
     def fit(self, X, y=None, **fit_params):
         return self
@@ -53,7 +58,7 @@ class SentenceLengthModeFeature(TransformerMixin):
             lengths.append(total_length)
             modes.append(Counter(lengths).most_common(1)[0][0])
 
-            return np.array(modes)
+        return np.array(modes).reshape(len(modes), 1)
 
 
 class FleschKincaidReadabilityEaseFeature(TransformerMixin):
@@ -74,7 +79,7 @@ class FleschKincaidReadabilityEaseFeature(TransformerMixin):
             ease_scores.append(
                 utils.flesch_kincaid_ease_score(number_of_sentences, number_of_words, number_of_syllables)
             )
-        return np.array(ease_scores).reshape(len(ease_scores),1)
+        return np.array(ease_scores).reshape(len(ease_scores), 1)
 
 
 class JaccardSimilarityAverageFeature(TransformerMixin):
@@ -94,6 +99,4 @@ class JaccardSimilarityAverageFeature(TransformerMixin):
                 scores.append(utils.jaccard(stemmed_sentence1, stemmed_sentence2))
 
             jaccard_scores.append(np.average(scores))
-        return np.array(jaccard_scores).reshape(len(jaccard_scores),1)
-
-
+        return np.array(jaccard_scores).reshape(len(jaccard_scores), 1)
